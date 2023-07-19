87fb3cde15eb36f643af196dfb278d0db667069b
Kacper Nalepa <kacper.nlp@wp.pl>
2023-07-19 17:33:12 +0200

    Created validation for login page

--- src/components/coaches/AppLoginCoachForm.vue
+++ src/components/coaches/AppLoginCoachForm.vue
@@ -1,28 +1,65 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmitForm">
     <app-form-input
-      v-mode="firstName"
+      v-model="firstName"
       label-text="First name:"
       input-id="firstName"
       error-text="First name is required"
       :is-not-valid="isFirtNameNotValid"
     ></app-form-input>
     <app-form-input
-      v-mode="password"
+      v-model="password"
       label-text="Password:"
       input-id="password"
       error-text="Password name is required"
       input-type="password"
-      :is-not-valid="isFirtNameNotValid"
+      :is-not-valid="isPasswordNotValid"
     ></app-form-input>
     <app-button class="center-btn">Login</app-button>
   </form>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import AppFormInput from "./AppFormInput.vue";
 
 const firstName = ref("");
 const password = ref("");
+const isFirtNameNotValid = ref(false);
+const isPasswordNotValid = ref(false);
+
+watch(firstName, (newValue, __) => {
+  if (isFirtNameNotValid.value && newValue.length > 0) {
+    isFirtNameNotValid.value = false;
+  }
+});
+
+watch(password, (newValue, __) => {
+  if (isPasswordNotValid.value && newValue.length > 0) {
+    isPasswordNotValid.value = false;
+  }
+});
+
+const onSubmitForm = () => {
+  if (isFormValid(firstName.value, password.value)) {
+    console.log("Valid pass");
+    return;
+  }
+
+  console.log("Valid not pass!");
+};
+
+const isFormValid = (firstName, password) => {
+  resetValidation();
+
+  if (!firstName && firstName.length < 3) isFirtNameNotValid.value = true;
+  if (!password && password.length < 3) isPasswordNotValid.value = true;
+
+  return !(isFirtNameNotValid.value || isPasswordNotValid.value);
+};
+
+const resetValidation = () => {
+  isFirtNameNotValid.value = false;
+  isPasswordNotValid.value = false;
+};
 </script>
